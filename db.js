06de2433d263fc0c04041df245ea2a51c3ea1e70
Sujal Rawat <sr702698@gmail.com>
2024-04-07 11:50:16 +0530

    new version

--- db.js
+++ db.js
@@ -8,7 +8,6 @@ const mongoURL=process.env.MONGODB_URL;
 
 //Set up MongoDB connection
 mongoose.connect(mongoURL,{
-    socketTimeoutMS:3000,
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
